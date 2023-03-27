cfa4f51e06f5d18ec03310a2da750d06d367207f
heylarissa <larissah.dandrade@gmail.com>
2023-03-27 18:45:45 -0300

    getopt

--- beale.c
+++ beale.c
@@ -11,7 +11,7 @@ int main (int argc, char *argv[]){
         switch (option){
             case 'e':
             // enconde
-                printf("ok");
+                
                 break;
             case 'd':
             //decode
