21fcee2b10e9ac09e881f8216b78cf24e7037b04
heylarissa <larissah.dandrade@gmail.com>
2023-04-14 01:42:34 -0300

    debug funcionando

--- beale.c
+++ beale.c
@@ -8,18 +8,57 @@
 #define FALSE 0
 #define LINESIZE 1024
 
-typedef struct value {
+typedef struct value
+{
     /* lista com valores referentes a letra*/
-    int value;                  // Código correspondente à letra
-    struct values *next;
+    int value; // Código correspondente à letra
+    struct value *next;
 } VALUE;
 
-typedef struct cifra {
+typedef struct dict
+{
     /* lista que contem a cifra completa */
-    char key;                   // letra
-    VALUE *codes;               // lista de códigos possíveis
-    struct cifra *next;
-} CIFRA;
+    char key;     // letra
+    VALUE *codes; // lista de códigos possíveis
+    struct dict *next;
+} DICT;
+
+void insertCode(char key, DICT *cifra, int code){
+    DICT *temp;
+    temp = cifra;
+    VALUE *codesTemp;
+    codesTemp = malloc(sizeof(VALUE));
+    while (temp->next!=NULL){
+        if (key == temp->key) {
+            // insere codigo na lista de códigos da chave
+            codesTemp = temp->codes;
+            while (codesTemp->next!= NULL){
+                codesTemp = codesTemp->next;
+            }
+            codesTemp->value = code;
+            return;
+        }
+        else {
+
+        }
+    }
+    return;
+}
+
+DICT *createNode(int code, char key, DICT *cifra){
+    DICT *temp;
+    temp = malloc(sizeof(DICT));
+    temp = NULL;
+    temp->codes = malloc(sizeof(VALUE));
+
+    temp = cifra;
+
+    insertCode(key, temp, code);
+
+    temp->key = key;
+    temp->codes->value = code;
+    printf("%d: %c\n", temp->codes->value, temp->key);
+}
 
 int main(int argc, char *argv[])
 {
@@ -32,14 +71,15 @@ int main(int argc, char *argv[])
 
     FILE *livroCifra;
 
-    CIFRA *cifra;
-    cifra = malloc(sizeof(CIFRA));
+    DICT *cifra;
+    cifra = malloc(sizeof(DICT));
+    cifra->codes = malloc(sizeof(VALUE));
 
     int option;
     int encode = FALSE;
     char word[LINESIZE + 1];
 
-    while ((option = getopt(argc, argv, "edbc:m:o:i:")) != -1)
+    while ((option = getopt(argc, argv, "edb:c:m:o:i:")) != -1)
     {
         switch (option)
         {
@@ -47,45 +87,49 @@ int main(int argc, char *argv[])
             // encode
             encode = TRUE;
             break;
+
         case 'd':
             // decode
             encode = FALSE;
-
             break;
+
         case 'b':
             // livro cifra
-            if (encode == TRUE) // tirar
-            {
-                livroCifra = fopen(optarg, "r");
-                if (!livroCifra)
-                {
-                    perror("Erro ao abrir arquivo x");
-                    exit(1); // encerra o programa com status 1
-                }
 
-                // lê as 10 primeiras linhas do arquivo
-                int i = 0;
-                while (!feof(livroCifra))
-                {
-                    fscanf(livroCifra, "%s[^\n]", word);
-                    printf("%d: %c\n", i, word[0]);
-                    i++;
-                }
+            livroCifra = fopen(optarg, "r");
+            if (!livroCifra)
+            {
+                perror("Erro ao abrir arquivo");
+                exit(1); // encerra o programa com status 1
+            }
+
+            int i = 0;
+            while (!feof(livroCifra))
+            {
+                fscanf(livroCifra, "%s[^\n]", word);
+                // Criar nodo da lista cifra
+                // Enquanto cria nodo da lista cifra, criar nodo da lista de values
+                cifra = createNode(i, word[0], cifra);
+                i++;
             }
             fclose(livroCifra);
 
             break;
+
         case 'm':
             printf("%s", optarg);
 
             // mensagem original;
             break;
+
         case 'o':
             // mensagem codificada
             break;
+
         case 'c':
             // decode
             break;
+
         case 'i':
             // decode
             break;
