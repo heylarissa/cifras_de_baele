84a53c0600a9bbf60f28605ef0d98ccabb1d91d0
heylarissa <larissah.dandrade@gmail.com>
2023-04-18 00:49:20 -0300

    criando o nodo

--- main.c
+++ main.c
@@ -8,9 +8,96 @@
 #define FALSE 0
 #define LINESIZE 1024
 
+typedef struct keys
+{
+    /* Armazena as chaves */
+    int value;
+    struct keys *next;
+} KEY;
 
+typedef struct keysDict
+{
+    /* Armazema o relacionamento da letra com as chaves possíveis */
+    char letter;
+    KEY *keys;
+    struct keysDict *next;
+} DICT;
 
+void checkFileOpening(FILE *file)
+{
+    if (!file)
+    {
+        perror("Erro ao abrir arquivo");
+        exit(1);
+    }
+}
 
+void initDictNode(DICT *dictionary)
+{
+    dictionary = malloc(sizeof(DICT));
+    dictionary->keys = malloc(sizeof(KEY));
+}
+
+KEY *appendKey(KEY *keys, int newKey)
+{
+    KEY *newNode;
+    newNode = malloc(sizeof(KEY));
+
+    newNode->value = newKey;
+    newNode->next = keys;
+
+    return newNode;
+}
+
+DICT *createNode(char letter, int key, DICT *dictionary)
+{
+    DICT *newDict = NULL, *aux;
+    aux = dictionary;
+    printf("%d %c\n", key, letter);
+
+    while (aux && aux->letter != letter)
+        aux = aux->next;
+
+    /*
+    Procura letra no dicionário
+    Se encontrar appendKey
+    Senão cria novo nodo
+    */
+
+    if (aux)
+    {
+        // aux->keys = appendKey(aux->keys);
+    }
+
+    return newDict;
+}
+
+// Lê o arquivo e carrega as chaves na memória
+DICT *loadKeys()
+{
+    DICT *dictionary;
+    dictionary = malloc(sizeof(DICT));
+    dictionary->keys = malloc(sizeof(KEY));
+
+    FILE *livroCifra;
+
+    char word[LINESIZE + 1];
+
+    livroCifra = fopen(optarg, "r");
+    checkFileOpening(livroCifra);
+
+    int key = 0;
+    while (!feof(livroCifra))
+    {
+        fscanf(livroCifra, "%s[^\n]", word);
+        dictionary = createNode(word[0], key, dictionary);
+
+        key++;
+    }
+    fclose(livroCifra);
+
+    return dictionary;
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,11 +108,10 @@ int main(int argc, char *argv[])
     //./beale  -d  -i MensagemCodificada  -c ArquivoDeChaves  -o MensagemDecodificada
     //./beale -d -i MensagemCodificada -b LivroCifra -o MensagemDecodificada
 
-    FILE *livroCifra;
+    DICT *dictionary;
 
     int option;
     int encode = FALSE;
-    char word[LINESIZE + 1];
 
     while ((option = getopt(argc, argv, "edb:c:m:o:i:")) != -1)
     {
@@ -46,19 +132,7 @@ int main(int argc, char *argv[])
             // livro cifra
 
             // gera as chaves a partir do livro cifra
-
-            livroCifra = fopen(optarg, "r");
-            if (!livroCifra)
-            {
-                perror("Erro ao abrir arquivo");
-                exit(1); // encerra o programa com status 1
-            }
-
-            while (!feof(livroCifra))
-            {
-                fscanf(livroCifra, "%s[^\n]", word);
-            }
-            fclose(livroCifra);
+            dictionary = loadKeys();
 
             break;
 
@@ -67,7 +141,7 @@ int main(int argc, char *argv[])
 
             // mensagem original;
             // a partir das chaves criadas codifica a mensagem
-            
+
             break;
 
         case 'o':
